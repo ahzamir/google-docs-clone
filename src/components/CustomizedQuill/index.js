648152559b50c355a25090af9433b902a629d147
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-05-21 15:26:25 +0430

    added a customized toolbar options for Quill

--- src/components/CustomizedQuill/index.js
+++ src/components/CustomizedQuill/index.js
@@ -2,10 +2,27 @@ import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 const CustomizedQuill = ({ value, onChange }) => {
+    const TOOLBAR_OPTIONS = [
+        ['undo', 'redo', 'print', 'spell-check', 'paint-format', 'zoom'],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'font': [] }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        ['bold', 'italic', 'underline', { 'color': [] }, { 'background': [] }],
+        ['link', 'comment', 'image'],
+        [{ 'align': [] }],
+        ['line-and-paragraph-spacing'],
+        ['checklist'],
+        [{ 'list': 'bullet' }, { 'list': 'ordered' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['clean']
+    ];
+
     return <ReactQuill
         theme="snow"
         value={value}
         onChange={onChange}
+        modules={{
+            toolbar: TOOLBAR_OPTIONS
+        }}
     />
 }
 
