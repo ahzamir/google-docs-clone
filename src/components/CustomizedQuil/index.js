c8a940fe666d00e9995a799e0e93b4657aaec670
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-05-21 14:55:14 +0430

    installed and imported the React Quill library and created its customized component

---
+++ src/components/CustomizedQuil/index.js
@@ -0,0 +1,10 @@
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+const CustomizedQuil = () => {
+    return (
+        <ReactQuill theme="snow" />
+    )
+}
+
+export default CustomizedQuil
