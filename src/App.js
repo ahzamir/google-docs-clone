43f73ce2f457bf20a47df9ec29b6eb8a308789ca
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-05-21 16:15:02 +0430

    added some basic design for the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TextEditor from './components/TextEditor';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <TextEditor />
     </div>
   );
