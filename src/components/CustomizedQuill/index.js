52ccd8ef473df36987833f90d429a6a6ef3d9ca1
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-05-21 15:04:13 +0430

    implemented the TextEditor component functionality using useState hook

--- src/components/CustomizedQuill/index.js
+++ src/components/CustomizedQuill/index.js
@@ -1,8 +1,12 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
-const CustomizedQuill = () => {
-    return <ReactQuill />;
+const CustomizedQuill = ({ value, onChange }) => {
+    return <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={onChange}
+    />
 }
 
 export default CustomizedQuill;
--- src/components/TextEditor/index.js
+++ src/components/TextEditor/index.js
@@ -1,7 +1,16 @@
-import CustomizedQuill from "../CustomizedQuill";
+import CustomizedQuill from '../CustomizedQuill';
+import { useState } from 'react';
 
 const TextEditor = () => {
-    return <CustomizedQuill />;
+    const [text, setText] = useState('');
+    return (
+        <div className="text-editor">
+            <CustomizedQuill
+                value={text}
+                onChange={setText}
+            />
+        </div>
+    );
 }
 
 export default TextEditor;
